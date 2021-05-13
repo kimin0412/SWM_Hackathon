c83f742f4f3899d094bb393841853d5deab8eee7
ntxq <bkwak@ust.hk>
2021-05-14 08:50:32 +0900

    {fix}: geocoding

--- frontend/src/components/organisms/Map/Map.js
+++ frontend/src/components/organisms/Map/Map.js
@@ -1,19 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import useMap from '../../../hooks/useMap';
-import { useDispatch } from 'react-redux';
-import { setBounds, setParkList } from '../../../store/parks';
-import axios from 'axios';
-// 모달
-// import swal from '@sweetalert/with-react';
-// import { SpotModal } from '../Map';
-// import useMarker from '../../../hooks/useMarker';
-import useCCTV from '../../../hooks/useCCTV';
-import useLights from '../../../hooks/useLights';
-import useFilter from '../../../hooks/useFilter';
-// import usePakrs from '../../../hooks/useParks';
+import React, { useEffect, useState } from "react";
+import useMap from "../../../hooks/useMap";
+import { useDispatch } from "react-redux";
+import { setBounds } from "../../../store/parks";
+import useFilter from "../../../hooks/useFilter";
 
-import createMarkers from '../../../hooks/createMarkers';
-import makeMarker from '../../../hooks/makeMarker';
+import makeMarker from "../../../hooks/makeMarker";
 
 // 마커 설정 : 기본위치-소마센터
 let nowPlace = {
@@ -22,130 +13,70 @@ let nowPlace = {
 };
 
 /* global kakao */
-const PARKMAX = 17735
+const PARKMAX = 17735;
 
 export const Map = ({ mobile }) => {
   const map = useMap();
   const [markerArr, setMarkerArr] = useState([]);
-  const [parkMarked, setParkMarked] = useState(Array(PARKMAX))
-  const [markerN, setMarkerN] = useState(0)
-  const [locationArr, setLocationArr] = useState([]);
-  const [infoArr, setInfoArr] = useState([]);
-  const cctvList = useCCTV();
-  const lightsList = useLights();
+  const [parkMarked, setParkMarked] = useState(Array(PARKMAX));
   const parksList = useFilter(markerArr, map);
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   console.log(parksList, cctvList, lightsList);
-  // }, [parksList, cctvList, lightsList]);
-
-  const getLocation = () => {
-    setLocationArr([getGeolocation()]);
-  };
-
-  const getGeolocation = () => {
-    // 위치 정보가 사용이 가능하면
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        nowPlace.x = position.coords.longitude;
-        nowPlace.y = position.coords.latitude;
-
-        getHCode(nowPlace);
-      });
-    }
-    map.setCenter(new kakao.maps.LatLng(nowPlace.y, nowPlace.x));
-    return nowPlace;
-  };
-  // const createMarker = () => {
-  //   setMarkerArr(createMarkers(locationArr, infoArr, map));
-  // };
-
-  useEffect(() => { }, []);
-
   useEffect(() => {
     try {
-      parksList['data'].forEach((element) => {
+      parksList["data"].forEach((element) => {
         if (!parkMarked[element.id]) {
-          let arr = parkMarked
+          let arr = parkMarked;
           arr[element.id] = true;
-          setParkMarked([...arr])
+          setParkMarked([...arr]);
           // setMarkerN(markerN + 1);
           const marker = makeMarker(element, map);
           setMarkerArr((prev) => {
             prev[element.id] = marker;
             return prev;
-          })
+          });
         }
       });
     } catch (e) {
       console.log(e);
     }
-  }, [parksList]);
-
-  // useEffect(() => {
-  //   if (100 < markerN) {
-  //     console.log('flush!!!!')
-  //     markerArr.forEach((element) => {
-  //       element.setMap(null);
-  //     })
-  //     setParkMarked(Array(PARKMAX))
-  //     setMarkerArr([])
-  //   }
-  // }, [markerN])
-
-  useEffect(
-    () => map && locationArr.length && infoArr.length
-    // && createMarker(),
-    [map, locationArr, infoArr]
-  );
+  }, [parksList, map, parkMarked]);
 
   useEffect(() => {
     if (map == null) return;
 
-    //Event listener for bounds change
-    kakao.maps.event.addListener(map, 'bounds_changed', () => {
-      dispatch(setBounds(map.getBounds()));
-    });
-    getLocation();
-  }, [map]);
+    const getGeolocation = () => {
+      // 위치 정보가 사용이 가능하면
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          nowPlace.x = position.coords.longitude;
+          nowPlace.y = position.coords.latitude;
 
-  const getHCode = function (position) {
-    var geocoder = new kakao.maps.services.Geocoder();
-
-    var callback = function (result, status) {
-      if (status === kakao.maps.services.Status.OK) {
-        // position.tmp = 20;
-        // position.pop = 0;
-
-        axios
-          .get('/api/weather?zone=' + result[1].code)
-          .then((Response) => {
-            // console.log(Response.data);
-            position.weather = Response.kmaList.wfKor;
-          })
-          .catch((Error) => {
-            console.log(Error);
-          });
+          map.setCenter(new kakao.maps.LatLng(nowPlace.y, nowPlace.x));
+        });
       } else {
+        map.setCenter(new kakao.maps.LatLng(nowPlace.y, nowPlace.x));
       }
     };
 
-    geocoder.coord2RegionCode(position.x, position.y, callback);
+    //Event listener for bounds change
+    kakao.maps.event.addListener(map, "bounds_changed", () => {
+      dispatch(setBounds(map.getBounds()));
+    });
+    getGeolocation();
+  }, [map, dispatch]);
 
-    return position;
-  };
+  const height = mobile ? "60vh" : "87vh";
 
-  const height = mobile ? '60vh' : '87vh';
   return (
     <div
       id="map"
       className="kakaomap"
       style={{
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         height: height,
       }}
     >
--- frontend/src/hooks/makeMarker.js
+++ frontend/src/hooks/makeMarker.js
@@ -1,31 +1,35 @@
-import { SpotModal } from '../components/organisms/Map/SpotModal.js';
-import swal from '@sweetalert/with-react';
-/* global kakao */
+import { SpotModal } from "../components/organisms/Map/SpotModal.js";
+import swal from "@sweetalert/with-react";
+
 export default function makeMarker(data, map) {
   // const [markerArr, setMarkerArr] = useState([])
   const { kakao } = window;
-  const imageSrc = 'https://i.ibb.co/cvVVdfc/parkIcon.png'
+  const imageSrc = "https://i.ibb.co/cvVVdfc/parkIcon.png";
 
   let marker = new kakao.maps.Marker({
     map: map,
     position: new kakao.maps.LatLng(data.lat, data.lon),
-    image: new kakao.maps.MarkerImage(imageSrc, new kakao.maps.Size(64, 64), new kakao.maps.Point(30, 20)),
+    image: new kakao.maps.MarkerImage(
+      imageSrc,
+      new kakao.maps.Size(64, 64),
+      new kakao.maps.Point(30, 20)
+    ),
   });
   const safety_idx = Math.round(data.safety_idx * 100) / 100;
   const info = new kakao.maps.InfoWindow({
     content: `<div style= "font: bold; padding:5px; font-size: 10px"><h2>${data.park_name}</h2><p>안전점수 ${safety_idx}</p><p>CCTV 수 ${data.cctv_cnt}</p><p>가로등 수 ${data.streetlamp}</p></div>`,
     removable: true,
   });
-  kakao.maps.event.addListener(marker, 'mouseover', function () {
+  kakao.maps.event.addListener(marker, "mouseover", function () {
     // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
     info.open(map, marker);
   });
 
-  kakao.maps.event.addListener(marker, 'mouseout', function () {
+  kakao.maps.event.addListener(marker, "mouseout", function () {
     // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
     info.close();
   });
-  kakao.maps.event.addListener(marker, 'click', () => {
+  kakao.maps.event.addListener(marker, "click", () => {
     swal(<SpotModal data={data} />);
   });
   return marker;
